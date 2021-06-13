5e05598ca067b34a33af8e0b76cb4e54a6f9e3a2
Trace Thompson <tracethmpsn@gmail.com>
2021-06-13 16:24:07 -0500

    photo stylez

--- pages/photography/[slug].js
+++ pages/photography/[slug].js
@@ -58,7 +58,7 @@ function PhotoContainer({ photoData }) {
     <Layout currentTitle={title}>
       <div className="w-full h-full md:pt-8 pb-16">
         <div className="relative w-full h-full flex content-center items-center">
-          <div className="h-full w-full md:pt-8 pb-16 px-8 mx-auto flex content-center items-center relative">
+          <div className="h-full w-full md:pt-8 pb-16 px-4 lg:px-8 mx-auto flex content-center items-center relative">
             <ImageGallery images={chunkedImages} {...{page, direction, setPage, paginate}}/>
           </div>
           <div className="flex flex-row w-full h-full absolute top-0 left-0">
