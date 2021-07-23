ee0446f5a2d849da68a26370189c1e3c00b3898c
Trace Thompson <tracethmpsn@gmail.com>
2021-07-23 18:51:18 -0400

    about page title

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import RichText from "../components/RichText"
 
 function AboutPage({ data, siteSettings }) {
   return (
-    <Layout siteSettings={siteSettings}>
+    <Layout siteSettings={siteSettings} title="About">
       <div className="h-full w-full lg:p-8 pt-0 pb-2 px-4 flex flex-col about">
         {siteSettings && siteSettings[0].about.map((block, i) => <RichText key={i} content={block} />)}
       </div>
